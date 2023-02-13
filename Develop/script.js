2c0c6ca6feae256ecf5d27f3b7415501807bd952
Cmbailey089 <christopherbailey089@gmail.com>
2023-02-13 02:08:13 -0500

    added code from challenge folder

---
+++ Develop/script.js
@@ -0,0 +1,68 @@
+// Assignment code here
+
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '123456789';
+var symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_\`{|}~';
+
+var min= 8;
+
+var max = 128;
+
+function generatePassword() {
+  let lowerOk = confirm("Lowercase letters?");
+  if (lowerOk) {
+    chars = lowerCase;
+  } else {
+  }
+  
+  let upperOK = confirm("Uppercase letters?");
+  if (upperOK) {
+    chars += upperCase;
+  } else {
+
+  }
+
+  let numbersOK = confirm("Include numbers?");
+  if (numbersOK) {
+    chars += numbers;
+  } else {
+
+  }
+
+  let symbolOK = confirm("Special characters?");
+  if (symbolOK) {
+    chars += symbols;
+  } 
+
+  let i = prompt("password length?");
+    if (i > 7 && i < 129) {
+      while (i > 0) {
+        password += chars[Math.floor(Math.random() * chars.length)];
+        i--;
+      }
+      return password;
+    }  
+    
+       else {
+         password = "Please try again.";
+         return password;
+       }
+      }
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
